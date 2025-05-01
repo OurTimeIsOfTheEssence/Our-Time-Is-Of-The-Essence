3fa8c2c16d6bb7454853cea49148939f09f4f8b6
Albin Stenhoff <99albste@gafe.molndal.se>
2025-05-01 21:27:40 +0200

    Inloggningsfunktion.

---
+++ src/OurTime.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using OurTime.WebUI.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurTime.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password, string returnUrl = null)
+        {
+            // Hämta användare från databasen:
+            var user = _db.Users
+                .FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                // Skapa cookie‐identitet
+                var claims = new[] { new Claim(ClaimTypes.Name, user.Username) };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity));
+
+                if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    return Redirect(returnUrl);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Fel användarnamn eller lösenord");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
