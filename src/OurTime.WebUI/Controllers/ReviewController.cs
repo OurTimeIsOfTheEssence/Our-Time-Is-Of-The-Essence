9dbe29a367a6acf39aa4b2a1d5e044848639332a
Albin Stenhoff <99albste@gafe.molndal.se>
2025-05-15 15:18:54 +0200

    SCRUM-73 <ReviewController>

---
+++ src/OurTime.WebUI/Controllers/ReviewController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurTime.WebUI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewController : ControllerBase
+    {
+        // POST api/review/save
+        [HttpPost("save")]
+        public IActionResult Save([FromBody] ReviewDto dto)
+        {
+            // Här kan du antingen spara till egen databas
+            // eller anropa den andra klassens API med HttpClient
+            return Ok(new { success = true });
+        }
+    }
+}
